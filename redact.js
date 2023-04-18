29a105ccd46182942793651653bd128f9bc0ef3a
parrotypoisson <71202170+parrotypoisson@users.noreply.github.com>
2023-04-17 23:33:36 -0400

    Update redact.js (#4)

--- redact.js
+++ redact.js
@@ -1,23 +1,23 @@
 Draw.loadPlugin(function(ui){
-	
-  mxResources.parse('redact=Redact Cells');
 
-  ui.actions.addAction('redact', function(){
-    var model = ui.editor.graph.model;
-    model.beginUpdate();
+    mxResources.parse('redact=Redact Cells');
 
-    for(var id in model.cells){
-      model.setValue(model.cells[id], "REDACTED");
-    }
+    ui.actions.addAction('redact', function(){
+        var model = ui.editor.graph.model;
+        model.beginUpdate();
 
-    model.endUpdate();
-  });
-	
-	var menu = ui.menus.get('extras');
-	var oldFunct = menu.funct;
-	
-	menu.funct = function(menu, parent){
-		oldFunct.apply(this, arguments);
-		ui.menus.addMenuItems(menu, ['-', 'redact'], parent);
-	};
+        for(var id in model.cells){
+            model.setValue(model.cells[id], "REDACTED");
+        }
+
+        model.endUpdate();
+    });
+
+    var menu = ui.menus.get('extras');
+    var oldFunct = menu.funct;
+
+    menu.funct = function(menu, parent){
+        oldFunct.apply(this, arguments);
+        ui.menus.addMenuItems(menu, ['-', 'redact'], parent);
+    };
 });
